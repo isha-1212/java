4c379b4d0fbb6f2cfa96872504835e57a21efb86
isha-1212 <ishuup1205@gmail.com>
2024-08-05 13:12:17 +0530

    Add files via upload

---
+++ practical1.java
@@ -0,0 +1,10 @@
+
+public class practical1 {
+
+    public static void main(String[] args) {
+        int age = 25;
+        System.out.println(age + " is the age of sam.");
+        System.out.println("\n BY ISHA PATEL_23DIT045");
+    }
+
+}
